a1b2edd43017f34d3df2c80299e5ffd327b790c9
Xiaohuan Li <xiaohuan.li@gmail.com>
2019-01-27 18:26:53 +0000

    adding vanilla error handler

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const   express = require("express"),
         app     = express();
-
 const PORT = process.env.PORT || 8080;
-
 const appetiteRoutes = require("./routes/appetites");
-app.use("/api/appetites", appetiteRoutes);
-
+const errorHandler = require("./handlers/error");
 const bodyParser = require("body-parser");
+
+app.use("/api/appetites", appetiteRoutes);
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+app.use(errorHandler);
+
 app.use(bodyParser.json());
 
 app.listen(PORT, process.env.IP, function(){
