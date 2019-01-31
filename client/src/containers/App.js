7bbc59f470ff9ab9884d874a6a5600be727b9f8c
Xiaohuan Li <xiaohuan.li@gmail.com>
2019-01-31 00:28:21 +0000

    minor fixes to follow up auth styling

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,8 +2,8 @@ import React from "react";// eslint-disable-line no-unused-vars
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Navbar";
-import Main from "./Main";// eslint-disable-line no-unused-vars
+import Navbar from "./Navbar";// eslint-disable-line no-unused-vars
+import Main from "./Main";
 
 const store = configureStore();
 
